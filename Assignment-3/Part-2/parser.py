f67347ac0f6e2a7c5f541310a16c91723f267dc0
imhess <79474657+imhess@users.noreply.github.com>
2023-12-01 18:32:57 -0800

    Added Mongo files in json format + Final Update

--- Assignment-3/Part-2/parser.py
+++ Assignment-3/Part-2/parser.py
@@ -20,41 +20,47 @@ def parse_page():
     page = collection.find_one({'_id': url})
     html_data = page['html']
 
-    db2 = client['parser']
-    collection2 = db2['professors']
+    client.close()
 
     bs = BeautifulSoup(html_data, 'html.parser')
 
     divs = bs.find_all('div', class_="clearfix")
 
     #name, title, office, email, and website
-    counter = 0
+    attributeList = []
+    id_counter = 0
     for div in divs:
+        counter = 0
         #print(div.find_all("p"))
         #div_id = counter
         for x in div.find_all(string=re.compile(r"^[A-Z]")):
             flag = False
-            y = x.find_next(text=lambda t: "" in t.text)
+            y = x.find_next(string=lambda t: "" in t.text)
             for i in y:
                 if (i.isalpha() or i.isnumeric()):
                     flag = True
             if flag == True:
                 y = y.lstrip(": ")
             else:
-                y = y.find_next(text=lambda t: "" in t.text)
-            print(x)
-            print(y)
-            print("-----")
-        #x = div(text=lambda t: "Title" in t.text)
-        #print(div())
-        #name = div.find('strong', string="").get_text(strip=True)
-        #title =
-        #office =
-        #email =
-        #website =
+                y = y.find_next(string=lambda t: "" in t.text)
+            y = y.replace(u'\xa0', u'')
+            y = y.rstrip(" ")
+            if counter != 0:
+                attributeList.append(y)
+            else:
+                attributeList.append(x)
+            counter += 1
+        if attributeList != []:
+            print(attributeList)
+            store({"_id": id_counter, "name": attributeList[0], "title": attributeList[1], "office": attributeList[2], "email": attributeList[4], "website": attributeList[5]})
+            id_counter += 1
+        attributeList.clear()
 
-        counter += 1
-
-
+def store(data):
+    client = MongoClient('mongodb://localhost:27017/')
+    db2 = client['parser']
+    collection2 = db2['professors']
+    collection2.update_one({'_id': data['_id']}, {'$set': data}, upsert=True)
+    client.close()
 
 parse_page()
