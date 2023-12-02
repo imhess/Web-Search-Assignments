59d95588d59a15df4774588e5cb18034940ae52b
imhess <79474657+imhess@users.noreply.github.com>
2023-12-01 16:59:39 -0800

    Update parser.py

--- Assignment-3/Part-2/parser.py
+++ Assignment-3/Part-2/parser.py
@@ -9,6 +9,7 @@
 #-----------------------------------------------------------*/
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+import re
 
 def parse_page():
     client = MongoClient('mongodb://localhost:27017/')
@@ -29,17 +30,30 @@ def parse_page():
     #name, title, office, email, and website
     counter = 0
     for div in divs:
-        for strong_tag in div.find_all('strong'):
-            text_data = strong_tag.nextSibling.next_sibling.strip()
-            print(f"Data between </strong> and <br/>: {text_data}")
+        #print(div.find_all("p"))
         #div_id = counter
+        for x in div.find_all(string=re.compile(r"^[A-Z]")):
+            flag = False
+            y = x.find_next(text=lambda t: "" in t.text)
+            for i in y:
+                if (i.isalpha() or i.isnumeric()):
+                    flag = True
+            if flag == True:
+                y = y.lstrip(": ")
+            else:
+                y = y.find_next(text=lambda t: "" in t.text)
+            print(x)
+            print(y)
+            print("-----")
+        #x = div(text=lambda t: "Title" in t.text)
+        #print(div())
         #name = div.find('strong', string="").get_text(strip=True)
         #title =
         #office =
         #email =
         #website =
 
-        #counter += 1
+        counter += 1
 
 
 
